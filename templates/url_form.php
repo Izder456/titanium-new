568cfc6119511036243186eee2ef06943883386a
shirt.js <2660574+shirtjs@users.noreply.github.com>
2018-10-24 10:02:46 -0400

    Update url_form.php

--- templates/url_form.php
+++ templates/url_form.php
@@ -67,7 +67,7 @@ function smart_select(ele){
 	<div style="width:800px; margin:0 auto;">
 	
 		<form method="post" action="school.php" target="_top" style="margin:0; padding:0;">
-			<input type="button" value="Home" onclick="window.location.href='index.php'">
+			<input type="button" value="Home" onclick="window.location.href='school.php'">
 			<input type="text" name="url" value="<?php echo $url; ?>" autocomplete="off">
 			<input type="hidden" name="form" value="1">
 			<input type="submit" value="Go">
